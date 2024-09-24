4ac0ae1d14d741fd59f134b8ecacf4d75a10bc57
Jason Lubken <email = jlubken@users.noreply.github.com>
2024-09-24 14:55:58 -0400

    Fix path

--- share/upgrade.levant.sh
+++ share/upgrade.levant.sh
@@ -8,16 +8,16 @@ zip="${src}.zip"
 dst="/usr/local/bin/${src}"
 lnk="/usr/local/bin/levant"
 
-wget -O ${zip} "https://releases.hashicorp.com/levant/${levant_version}/${levant_version}_linux_amd64.zip"
+wget -O ${zip} "https://releases.hashicorp.com/levant/${levant_version}/levant_${levant_version}_linux_amd64.zip"
 sudo unzip ${zip}
 sudo mv levant ${src}
 
-for node in "${nodes[@]}";
+for node in "${jumps[@]}";
 do
         download_levant $node $src $lnk $dst
 done;
 
-for node in "${nodes[@]}";
+for node in "${jumps[@]}";
 do
         upgrade_levant $node $lnk $dst
 done;
