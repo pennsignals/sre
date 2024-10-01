4e18b5177c7c75ae8994dd49c52efdf3e59aa83b
Jason Lubken <jlubken@users.noreply.github.com>
2024-10-01 09:45:14 -0400

    Wait after restarting nomad service after upgrade

--- share/upgrade.nomad.sh
+++ share/upgrade.nomad.sh
@@ -3,6 +3,9 @@ set -euxo pipefail
 
 source vars.sh
 
+servers="${1:-true}"
+echo "nomad upgrade servers: ${servers}"
+
 src="nomad-${nomad_version}"
 zip="${src}.zip"
 dst="/usr/local/bin/${src}"
@@ -17,15 +20,25 @@ do
         download_nomad $node $src $lnk $dst
 done;
 
-for node in "${nodes[@]}";
+if servers; then
+        for node in "${nomads[@]}";
+        do
+                upgrade_nomad $node $lnk $dst
+        done;
+
+        for nomad in "${nomads[@]}";
+        do
+                restart_nomad $nomad
+                echo "Allow time for server to rejoin"
+                sleep 10
+        done;
+fi
+
+for node in "${minions[@]}";
 do
         upgrade_nomad $node $lnk $dst
 done;
 
-for nomad in "${nomads[@]}";
-do
-        restart_nomad $nomad
-done;
 
 for minion in "${minions[@]}";
 do
