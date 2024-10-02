65b0400db508eab42ead7ef518de4f3ec6783f11
Jason Lubken <jlubken@users.noreply.github.com>
2024-10-02 13:55:55 -0400

    Add autofs install

---
+++ cloud.staging/autofs.all.nodes.sh
@@ -0,0 +1,1 @@
+../share/autofs.all.nodes.sh
---
+++ share/autofs.all.nodes.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+source vars.sh
+
+nojumps=($(comm -23 <(printf "%s\n" "${nodes[@]}" | sort) <(print "%s\n" "${jumps[@]}" | sort)))
+
+for node in "${nodes[@]}";
+do
+        autofs $node $jumps[0]
+done;
