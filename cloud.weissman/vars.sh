fc4074c407b11c4fc640b79b577111e71ced3b62
Jason Lubken <jlubken@users.noreply.github.com>
2025-03-05 14:48:32 -0500

    Spelling on resource group

--- cloud.weissman/vars.sh
+++ cloud.weissman/vars.sh
@@ -7,6 +7,6 @@ ssh_key=".ssh/acs_key"
 falcon_sensor_version="7.19.0-17221"
 falcon_sensor_cid="EB648EE3E2ED4C688A4615C987F4EAEB-BF"
 subscription="DATASCIENCE-DEVELOPMENT"
-resource_group="translational_weissman"
+resource_group="translational-weissman"
 
 source lib.sh
